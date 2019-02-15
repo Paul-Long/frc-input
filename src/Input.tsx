515473919ea84e56791c75824507a7278678dfb3
Paul <baolong.hou@sumscope.com>
2019-02-15 14:26:58 +0800

    init

---
+++ src/Input.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface IInputProps {
+
+}
+class Input extends React.Component<IInputProps> {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Input;
